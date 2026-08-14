4963c6d22e514a0bbaa965facc52f64480b83b0c
goodfoodtruck <mielatomique@protonmail.com>
2026-08-14 22:16:34 +0200

    recalculate engine and steering properties (1)

--- vehicle/components/vehicle_steering.gd
+++ vehicle/components/vehicle_steering.gd
@@ -2,8 +2,8 @@ class_name VehicleSteering
 extends Node
 
 @export var _max_steering_angle_deg := 35.0
-@export var _steering_speed_deg := 180.0
-@export var _return_speed_deg := 260.0
+@export var _steering_speed_deg := 90.0
+@export var _return_speed_deg := 180.0
 
 @onready var _max_steering_angle := deg_to_rad(_max_steering_angle_deg)
 @onready var _steering_speed := deg_to_rad(_steering_speed_deg)
@@ -14,9 +14,9 @@ extends Node
 
 func update(delta: float, axis: float) -> void:
 	var target := clampf(axis, -1.0, 1.0) * _max_steering_angle
-	var rate := _steering_speed if not is_zero_approx(axis) else _return_speed
+	var rate := _steering_speed if not is_zero_approx(axis) else (_return_speed * (3 * PI))
 	_steering_angle = move_toward(_steering_angle, target, rate * delta)
 
 
 func get_yaw_delta(delta: float, speed: float, max_speed: float) -> float:
-	return _steering_angle * PI * (speed / max_speed) * delta
+	return _steering_angle * (3 * PI) * (speed / max_speed) * delta
